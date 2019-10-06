f16bd1117dbf2d7adcda66b83f93d7d21a5e2ab4
namig <guseinovnamig41@gmail.com>
2019-10-06 21:06:30 +0300

    first commit

---
+++ test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE months (id int, name varchar(10), days int);
+
+INSERT INTO months VALUES (1, 'May', 31);
+INSERT INTO months VALUES (2, 'May', 31);
+INSERT INTO months VALUES (3, 'May', 31);
+INSERT INTO months VALUES (4, 'May', 31);
+INSERT INTO months VALUES (5, 'May', 31);
+
+SELECT * FROM months;
